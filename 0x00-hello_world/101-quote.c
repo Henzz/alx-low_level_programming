534af027c42f7da9e91d60bbbfa03c0980dabb6c
Henzz <khenok53@gmail.com>
2022-06-08 08:00:52 -0700

    printf and without printf & puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -12,6 +12,6 @@ int main(void)
 {
 	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
-	syscall(SYS_write, "\n", msg, sizeof(msg));
+	syscall(SYS_write, 1, msg, sizeof(msg));
 	return (1);
 }
