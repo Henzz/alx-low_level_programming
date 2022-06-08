e74cb98cd08d05ffd64bc20663e944fee6f19700
Henzz <khenok53@gmail.com>
2022-06-08 08:10:48 -0700

    without

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,8 +10,8 @@
  */
 int main(void)
 {
-	const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	syscall(SYS_write, 1, msg, sizeof(msg));
+	syscall(SYS_write, msg, sizeof(msg));
 	return (1);
 }
