80a5c6453c34e8cbfa4f2a7c46ad909932d18dcb
Henzz <khenok53@gmail.com>
2022-06-08 07:47:53 -0700

    print without printf and puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,8 @@
  */
 int main(void)
 {
-	const char msg[] = "and that piece of art is useful\" - Dora korpar, 2015-10-19\n";
+	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 	syscall(SYS_write, 1, msg, sizeof(msg));
-	return 1;
+	return (1);
 }
