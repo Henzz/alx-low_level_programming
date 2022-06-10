4cca979aa960f96179d448f4330d675a750ec340
Henzz <khenok53@gmail.com>
2022-06-10 05:26:56 -0700

    possible combination of single digits

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+/*#!/bin/bash*/
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar(36);
+	putchar('\n');
+	return (0);
+}
