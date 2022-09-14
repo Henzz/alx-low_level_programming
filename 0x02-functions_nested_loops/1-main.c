2c9bad5ed16425b694bdc9ddef05947df29363ac
Henzz <khenok53@gmail.com>
2022-09-13 18:28:25 -0700

    computes the absolute value of an integer

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -13,14 +14,14 @@ int main(void)
 	 * print_alphabet_x10();
 	 */
 	/*
-	 * r = _islower('H');
+	r = _islower('H');
 	_putchar(r + '0');
 	r = _islower('o');
 	_putchar(r + '0');
 	r = _islower(108);
 	_putchar('\n'); 
 	*/
-
+	/*
 	r = print_sign(98);
 	_putchar(',');
 	_putchar(' ');
@@ -41,5 +42,16 @@ int main(void)
 	_putchar(' ');
 	_putchar(r + '0');
 	_putchar('\n');
+	*/
+
+	
+	r = _abs(-1);
+	printf("%d\n", r);
+	r = _abs(0);
+	printf("%d\n", r);
+	r = _abs(1);
+	printf("%d\n", r);
+	r = _abs(98);
+	printf("%d\n", r);
 	return (0);
 }
