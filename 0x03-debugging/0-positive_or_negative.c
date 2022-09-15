7e763acb24137407783b10e9d9adee1e039a8cbd
Henzz <khenok53@gmail.com>
2022-09-15 02:35:32 -0700

    c debuggin checking a number is positive or negative

--- 0x03-debugging/0-positive_or_negative.c
+++ 0x03-debugging/0-positive_or_negative.c
@@ -1,4 +1,3 @@
-/*#!/bin/bash*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -8,23 +7,18 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+void positive_or_negative(int i)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
+	if (i > 0)
 	{
-		printf("%d is positive\n", n);
+		printf("%d is positive\n", i);
 	}
-	else if (n < 0)
+	else if (i < 0)
 	{
-		printf("%d is negative\n", n);
+		printf("%d is negative\n", i);
 	}
-	else if (n == 0)
+	else if (i == 0)
 	{
-		printf("%d is zero\n", n);
+		printf("%d is zero\n", i);
 	}
-	return (0);
 }
