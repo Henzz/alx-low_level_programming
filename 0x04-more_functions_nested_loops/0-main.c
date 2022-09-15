1ae5766f8b348f4f2ddf7b67c09b83b0b9fc3d75
Henzz <khenok53@gmail.com>
2022-09-15 15:18:55 -0700

    multiplies two integers

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -8,11 +8,14 @@
  */
 int main(void)
 {
-	char c;
+	printf("%d\n", mul(98, 4096));
+	printf("%d\n", mul(-402, 4096));
+	/*char c;
 
 	c = '0';
 	printf("%c: %d\n", c, _isdigit(c));
 	c = 'a';
 	printf("%c: %d\n", c, _isdigit(c));
+	*/
 	return (0);
 }
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * mul - multiplies to integers
+ * @a: integer
+ * @b: integer
+ * Return: multiplication of a and b
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = (a * b);
+	return (result);
+}
