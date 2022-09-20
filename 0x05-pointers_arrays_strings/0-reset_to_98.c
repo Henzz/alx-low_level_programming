83e67f2d1ce4a429a38fbe84e30d477525eab2af
Henzz <khenok53@gmail.com>
2022-09-19 18:07:01 -0700

    pointer reset to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,7 @@
-/*/bin/bash*/
 #include <stdio.h>
 
 /**
  * reset_to_98 - change pointer value
- *
  * @n: pointer parameter
  *
  * Return: Nothing.
@@ -11,26 +9,4 @@
 void reset_to_98(int *n)
 {
 	*n = 98;
-}
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	int n;
-
-	n = 402;
-	putchar('n');
-	putchar('=');
-	putchar(n/10 + '0');
-	putchar('\n');
-	reset_to_98(&n);
-	putchar('n');
-	putchar('=');
-	putchar(n%10 + '0');
-	putchar('\n');
-	return (0);
 }
