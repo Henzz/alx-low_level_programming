f04c97ddcad2ef084d3f8dac8e587270ae170370
Henzz <khenok53@gmail.com>
2022-09-21 01:00:30 -0700

    concatenates two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include <string.h>
+
+/**
+ * *_strcat - concatenates two strings
+ * @dest: value of first string
+ * @src: value of second string
+ *
+ * Return: concatenated string.
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
