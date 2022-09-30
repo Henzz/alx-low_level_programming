89a65400d4b4befcfdf09dc656b6a33d2e122b7b
Henzz <khenok53@gmail.com>
2022-09-30 16:59:31 -0700

    a program that adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,12 +1,3 @@
-#ifdef UNUSED
-#elif defined(__GNUC__)
-# define UNUSED(x) { UNUSED_ ## x __attribute__((unused)) }
-#elif defined(__LCLINT__)
-# define UNUSED(x) /*@unused@*/ x
-#else
-# define UNUSED(x) x
-#endif
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <ctype.h>
@@ -18,7 +9,7 @@
  *
  * Return: Always 0(Success).
  */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
 	int i, sum = 0;
 
