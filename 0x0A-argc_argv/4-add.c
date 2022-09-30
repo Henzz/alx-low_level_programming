26dab527d181f8b26e3e36b57c0c858e5d88f327
Henzz <khenok53@gmail.com>
2022-09-30 16:23:02 -0700

    a program that adds positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,47 @@
+#ifdef UNUSED
+#elif defined(__GNUC__)
+# define UNUSED(x) { UNUSED_ ## x __attribute__((unused)) }
+#elif defined(__LCLINT__)
+# define UNUSED(x) /*@unused@*/ x
+#else
+# define UNUSED(x) x
+#endif
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - prints the number of arguments passed
+ * @argc: number of arguments
+ * @argv: strings of the arguments
+ *
+ * Return: Always 0(Success).
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	int i, sum = 0;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (atoi(argv[i]) != 0)
+			{
+				sum += atoi(argv[i]);
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		printf("%d\n", sum);
+		return (0);
+	}
+	else
+	{
+		printf("0\n");
+		return (0);
+	}
+}
