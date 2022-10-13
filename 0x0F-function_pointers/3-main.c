b6b1d060ef1fb7c717ce09c208de38eec701bde9
Henzz <khenok53@gmail.com>
2022-10-12 19:33:50 -0700

    a program that performs simple operations of add, sub, mul, div & mod of given two numbers

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 	int num1, num2, calc;
 	char *operator;
 
-	if (argc < 4 || argc > 4)
+	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
@@ -21,7 +21,13 @@ int main(int argc, char *argv[])
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 	operator = argv[2];
-	if (strcmp(operator, "+") != 0 &&
+	if (get_op_func(operator) == NULL && operator[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	/*
+	 * if (strcmp(operator, "+") != 0 &&
 			strcmp(operator, "-") != 0 &&
 			strcmp(operator, "*") != 0 &&
 			strcmp(operator, "/") != 0 &&
@@ -30,7 +36,8 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(99);
 	}
-	if (num2 == 0 && (strcmp(operator, "/") == 0 || strcmp(operator, "%") == 0))
+	*/
+	if (num2 == 0 && ((*operator == '/') || (*operator == '%')))
 	{
 		printf("Error\n");
 		exit(100);
