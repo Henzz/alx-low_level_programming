a0f29b0d91b252bc571ec4113e36afe460a97d32
Henzz <khenok53@gmail.com>
2022-11-05 16:06:24 +0300

    a function that copies file from one to another

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -57,7 +57,7 @@ int main(int argc, char *argv[])
 		if (file_to == -1 || w == -1)
 		{
 			dprintf(STDERR_FILENO,
-					"Error: can't write to %s\n", argv[2]);
+					"Error: Can't write to %s\n", argv[2]);
 			free(buffer);
 			exit(99);
 		}
