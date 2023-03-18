1fdbf6243ae929a0a665ee4e69c6920f8f77699f
Henzz <khenok53@gmail.com>
2023-03-18 04:32:46 +0300

    betty style resolved

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 
-/*
+/**
  * dlistint_len - returns the number of elements in a linked list
  * @h: list of nodes
- * return: number of elements
+ * Return: number of nodes
  */
 size_t dlistint_len(const dlistint_t *h)
 {
