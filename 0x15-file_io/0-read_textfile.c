cbe1e52e98a685ae80137db9ddd50029c242bcda
Henzz <khenok53@gmail.com>
2023-05-12 23:02:39 +0300

    file io task 1 done

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX
+ * standard output according given number of letters.
+ * @filename: file name to be read
+ * @letters: number of letters to print
+ *
+ * Return: actual number of letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t o, r, w;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(letters * sizeof(char));
+	if (buffer == NULL)
+		return (0);
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+	if (o == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(buffer);
+		return (0);
+	}
+	free(buffer);
+	close(o);
+	return (w);
+}
