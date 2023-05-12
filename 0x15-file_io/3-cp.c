6c2290b5be3e10d760a941b8e6ee638af809e101
Henzz <khenok53@gmail.com>
2023-05-13 01:08:18 +0300

    file io task 3 modified

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,23 @@
 #include "main.h"
 
+void close_file(int fd);
+
+/**
+ * close_file - closes file descriptors.
+ * @fd: The file descriptor to be closed.
+ */
+void close_file(int fd)
+{
+	int c;
+
+	c = close(fd);
+	if (c == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
 /**
  * main - copies file content from one file to another
  * @ac: number of input
@@ -18,6 +36,10 @@ int main(int ac, char **av)
 	}
 	buffer = malloc(1024 * sizeof(char));
 	if (buffer == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
 		return (0);
 	file_from = open(av[1], O_RDONLY);
 	r = read(file_from, buffer, 1024);
@@ -25,14 +47,14 @@ int main(int ac, char **av)
 	do {
 		if (file_from == -1 || r == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[0]);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 			free(buffer);
 			exit(98);
 		}
 		w = write(file_to, buffer, r);
 		if (file_to == -1 || w == -1)
 		{
-			dprintf(2, "Error: Can't write to %s\n", av[1]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 			free(buffer);
 			exit(99);
 		}
@@ -40,7 +62,7 @@ int main(int ac, char **av)
 		file_to = open(av[2], O_WRONLY | O_APPEND);
 	} while (r > 0);
 	free(buffer);
-	close(file_from);
-	close(file_to);
+	close_file(file_from);
+	close_file(file_to);
 	return (0);
 }
