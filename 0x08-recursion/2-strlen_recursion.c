ead2526b9f962e795c702783e9018c3dda35b1f6
Henzz <khenok53@gmail.com>
2023-06-09 01:46:03 +0300

    task 2 done str length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - counts the number of characters in a string
+ * @s: the string to be counted
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
