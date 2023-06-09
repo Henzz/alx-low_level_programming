16aa30b505f8fdf3ab5a0dbe47b75598f4554996
Henzz <khenok53@gmail.com>
2023-06-09 03:50:22 +0300

    task 100 done palindrome

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,5 +10,7 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int sqrtSearch(int low, int high, int N);
 int is_prime_number(int n);
+int is_palindrome(char *s);
+int isPalRec(char str[], int s, int e);
 
 #endif /* MAIN_H */
