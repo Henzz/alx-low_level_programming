4abc3c26fd4ae5dacbbe9d66199ea2d638afa824
Henzz <khenok53@gmail.com>
2023-08-08 21:31:06 +0300

    modified return

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,21 +10,23 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	ssize_t o, lettercount;
+	ssize_t o, w, lettercount;
 
 	if (filename == NULL)
-		return (-1);
+		return (0);
 	o = open(filename, O_TRUNC | O_CREAT | O_WRONLY, 0600);
 	if (o == -1)
-		return (-1);
+		return (0);
 	lettercount = 0;
 	if (text_content != NULL)
 	{
 		while (text_content[lettercount] != '\0')
 			lettercount++;
 	}
-	write(o, text_content, lettercount);
+	w = write(o, text_content, lettercount);
+	if (w == -1)
+		return (0);
 	close(o);
-	return (1);
+	return (w);
 
 }
