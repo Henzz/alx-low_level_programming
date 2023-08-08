6b5b34a913fa1f0318a0cb8ec69fedf5f58b2f0e
Henzz <khenok53@gmail.com>
2023-08-08 21:33:25 +0300

    reverting back

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,23 +10,21 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	ssize_t o, w, lettercount;
+	ssize_t o, lettercount;
 
 	if (filename == NULL)
-		return (0);
+		return (-1);
 	o = open(filename, O_TRUNC | O_CREAT | O_WRONLY, 0600);
 	if (o == -1)
-		return (0);
+		return (-1);
 	lettercount = 0;
 	if (text_content != NULL)
 	{
 		while (text_content[lettercount] != '\0')
 			lettercount++;
 	}
-	w = write(o, text_content, lettercount);
-	if (w == -1)
-		return (0);
+	write(o, text_content, lettercount);
 	close(o);
-	return (w);
+	return (1);
 
 }
