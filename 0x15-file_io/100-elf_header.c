36a22df226815926fb6191e7755751bddb6720e4
Henzz <khenok53@gmail.com>
2023-08-08 05:49:48 +0300

    file io done

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <elf.h>
+
+/**
+ * main - Displays the information contained in the ELF header
+ * at the start of an ELF file.
+ * @argc: Number of arguments.
+ * @argv: Array of arguments names.
+ * Return: 
+ */
