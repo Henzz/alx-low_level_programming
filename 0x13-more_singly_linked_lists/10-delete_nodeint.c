b88f6a1d4063fd24a7a232c7573e0fd9417cc492
Henzz <khenok53@gmail.com>
2023-10-25 19:54:31 +0300

    task 10 modified

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -22,7 +22,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (-1);
 
 	copy = *head;
-	while (i < (index - 1))
+	while (i <= index)
 	{
 		if (copy == NULL || copy->next == NULL)
 			return (-1);
