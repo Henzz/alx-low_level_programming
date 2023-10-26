3164f0c76300fbe16687f82c620e2bb11e23bb1c
Henzz <khenok53@gmail.com>
2023-10-26 23:47:51 +0300

    task 1 modified to without %, / or malloc

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -5,23 +5,8 @@
  */
 void print_binary(unsigned long int n)
 {
-	int binary[32], index = 0, i;
+	if (n > 1)
+		print_binary(n >> 1);
 
-	if (n == 0)
-		_putchar(0 + '0');
-
-	while (n > 0)
-	{
-		i = n % 2;
-		binary[index] = n % 2;
-		n /= 2;
-		index++;
-	}
-
-	i = index - 1;
-	while (i >= 0)
-	{
-		_putchar(binary[i] + '0');
-		i--;
-	}
+	_putchar((n & 1) + '0');
 }
