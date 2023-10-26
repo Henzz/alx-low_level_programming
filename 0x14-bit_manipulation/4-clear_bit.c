412c59ef72bc1c7cd442e676040f0ac039c5c981
Henzz <khenok53@gmail.com>
2023-10-27 00:43:16 +0300

    task 4 done

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,22 +1,20 @@
 /**
- * set_bit - sets the value of a bit to 1 at given index.
+ * clear_bit - sets the value of a bit to 0 at given index.
  * @n: the number to set the bit to.
  * @index: the index position value.
  *
  * Return: if success 1, if failure -1.
  */
-int set_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int mask;
 
 	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
 
-	/* create a mask with only the bit at the given index set to 1 */
+	/* create a mask with only the bit at the given index set to 0 */
 	mask = 1 << index;
 	/* clear the bit at the given index in the number */
 	(*n) = (*n) & ~mask;
-	/* set the bit at the given index to the desired value */
-	(*n) = (*n) | (1 << index);
 	return (1);
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -10,7 +10,9 @@ unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 /* gets the value of a bit at a given index */
 int get_bit(unsigned long int n, unsigned int index);
-/* sets the value of a bit at a given index */
+/* sets the value of a bit to 1 at a given index */
 int set_bit(unsigned long int *n, unsigned int index);
+/* sets the value of a bit to 0 at a given index */
+int clear_bit(unsigned long int *n, unsigned int index);
 
 #endif /* MAIN_H */
