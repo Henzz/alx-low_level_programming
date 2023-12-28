2e73f747f4bdce9a5199352f0b04f927e0bd88b2
Henzz <khenok53@gmail.com>
2023-12-29 00:39:30 +0300

    task 3 updated

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -42,6 +42,27 @@ int update_node(hash_node_t *item, const char *key, const char *value)
 }
 
 /**
+ * create_node - creates a new node.
+ * @key: the new key.
+ * @value: the new value.
+ * Return: on success the created new node, otherwise NULL.
+ */
+hash_node_t *create_node(const char *key, const char *value)
+{
+	hash_node_t *new_node;
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = NULL;
+
+	return (new_node);
+}
+
+/**
  * hash_table_set - adds an element to a hash table.
  * @ht: the hash table you want ot add or update the key/value to.
  * @key: the key.
@@ -51,44 +72,35 @@ int update_node(hash_node_t *item, const char *key, const char *value)
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new_node;
-	int j;
-	size_t index;
+	unsigned long int index;
+	hash_node_t *new_node, *current_node;
 
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (0);
 
+	/* Check if key already exists and update the value */
 	index = key_index((const unsigned char *)key, ht->size);
-	new_node = malloc(sizeof(hash_node_t));
+	current_node = ht->array[index];
+	while (current_node != NULL)
+	{
+		if (strcmp(current_node->key, key) == 0)
+		{
+			free(current_node->value);
+			current_node->value = strdup(value);
+			return (1);
+		}
+		current_node = current_node->next;
+	}
+
+	/* Key doesn't exist, create a new node */
+	new_node = create_node(key, value);
 	if (new_node == NULL)
 		return (0);
 
-	new_node->key = strdup(key);
-	new_node->value = strdup(value);
-	new_node->next = NULL;
-	if (new_node->key == NULL || (new_node->value == NULL && value != NULL))
-	{
-		free_node(new_node);
-		return (0);
-	}
-	if (ht->array[index] == NULL)
-	{
-		/* No collision, insert the new node at the beginning of the list */
-		ht->array[index] = new_node;
-	}
-	else
-	{
-		/* collision occurred, add the new node at the beginning of the list */
-		j = update_node(ht->array[index], key, value);
-		if (j == 1)
-		{
-			free_node(new_node);
-			return (1);
-		}
-
-		new_node->next = ht->array[index];
-		ht->array[index] = new_node;
-	}
+	/* Insert the new node at the beginning of the list */
+	index = key_index((const unsigned char *)key, ht->size);
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
 
 	return (1);
 }
