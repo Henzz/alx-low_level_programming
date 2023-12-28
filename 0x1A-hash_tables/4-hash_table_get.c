9b3b2ee63893ab0862cda5eb9212daf65ce70572
Henzz <khenok53@gmail.com>
2023-12-28 23:50:06 +0300

    task 4 update

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -18,10 +18,11 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	index = key_index((const unsigned char *)key, ht->size);
 	item = (hash_node_t *)ht->array[index];
 
-	if (item != NULL)
+	while (item != NULL)
 	{
 		if (strcmp(item->key, key) == 0)
 			return (item->value);
+		item = item->next;
 	}
 
 	return (NULL);
